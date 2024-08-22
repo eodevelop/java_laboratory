463038fbb38604f121263cb7943983ef5e965299
eodevelop <eodevelop@naver.com>
2024-08-22 21:44:24 +0900

    프로젝트 생성

---
+++ src/main/java/com/minjae/java_laboratory/JavaLaboratoryApplication.java
@@ -0,0 +1,13 @@
+package com.minjae.java_laboratory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaLaboratoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaLaboratoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/minjae/java_laboratory/JavaLaboratoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.minjae.java_laboratory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaLaboratoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
